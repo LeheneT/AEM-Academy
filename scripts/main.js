413046b66b66dff364d8716cf2f849fc11329868
Panainte Andrei <66826911+andrei645@users.noreply.github.com>
2022-10-07 18:21:12 +0300

    Desktop and mobile designs
    Last changes, desktop and mobiles designs ready, with all the validation required.
    To do : tablet optimization, very little positioning details

--- scripts/main.js
+++ scripts/main.js
@@ -7,6 +7,8 @@ const btn = document.querySelector(".btn");
 const nameErr = document.querySelector(".error-for-name");
 const numberErr = document.querySelector(".error-for-number");
 const numberPlaceholder = document.querySelector(".card-front-number");
+const dateErr = document.querySelector(".error-for-date");
+const cvcErr = document.querySelector(".error-for-cvc");
 
 let arr = [];
 
@@ -15,6 +17,7 @@ btn.addEventListener("click", () => {
     nameValidation();
     numberValidation();
     dateValidation();
+    cvcValidation();
 });
 
 function nameValidation(){
@@ -45,6 +48,12 @@ function numberValidation(){
         break;
     }
   }
+    for(let i = 0; i < numberArr.length; i++){
+        if(i === 4 || i === 9 || i === 14){
+            numberArr.splice(i, 0, " ");
+        }
+    }
+  console.log(numberArr);
   if(ok === 0){
     cardNumber.classList.add("wrong");
     numberErr.classList.remove("hidden");
@@ -54,16 +63,40 @@ function numberValidation(){
     cardNumber.classList.remove("wrong");
     numberErr.classList.remove("show");
     numberErr.classList.add("hidden");
-    document.querySelector(".card-front-number").innerHTML = cardNumber.value;
+    document.querySelector(".card-front-number").innerHTML = numberArr.join("");
   }
 }
 
 function dateValidation(){
+    let dateRegex = /^[0-9][0-9]$/;
     if (dateM.value.length != 2 || dateY.value.length != 2){
         dateM.classList.add("wrong");
         dateY.classList.add("wrong");
+        dateErr.classList.remove("hidden");
+        dateErr.classList.add("show");
+    } else if (!dateRegex.test(dateM.value) || !dateRegex.test(dateY.value) || dateM.value === '00' || dateM.value > '12') {
+        dateM.classList.add("wrong");
+        dateY.classList.add("wrong");
+        dateErr.classList.remove("hidden");
+        dateErr.classList.add("show");
     } else {
         dateM.classList.remove("wrong");
         dateY.classList.remove("wrong");
+        dateErr.classList.add("hidden");
+        document.querySelector(".card-front-date").innerHTML = dateM.value + " / " + dateY.value;
+    }
+}
+
+function cvcValidation(){
+    let cvcRegex = /^[0-9][0-9][0-9]$/;
+    if(cardCvc.value.length != 3 || !cvcRegex.test(cardCvc.value)){
+        cardCvc.classList.add("wrong");
+        cvcErr.classList.remove("hidden");
+        cvcErr.classList.add("show");
+    } else{
+        cardCvc.classList.remove("wrong");
+        cvcErr.classList.add("hidden");
+        cvcErr.classList.remove("show");
+        document.querySelector(".card-cvc").innerHTML = cardCvc.value;
     }
 }
